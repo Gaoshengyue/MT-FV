81f01cf67b71dd5e5ea6bab196e54fca5cc3d00f
Gaoshengyue <Gaoshengyue@github.com>
2019-08-22 17:07:33 +0800

    update local.css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="hello">
+  <div class="hello" style="height: calc(90vh)">
     <div v-if="show">
 <spinner :type="types" :size="size" class="center-spin"></spinner>
     <strong class="center-spin" style="font-size: 100%">我的猫开发中</strong>
       </div>
-    <div v-else class="">
+    <div v-else class=" container-fluid;" >
+      <div class="row" style="background-color: lightcoral;">
+        <div class="col-lg-2 col-sm-2 col-md-2 col-xs-2">
+
+        </div>
+        <div class="col-lg-3 col-sm-3 col-md-3 col-xs-3">
+
+        </div>
+        <div class="col-lg-7 col-sm-7 col-md-7 col-xs-7">
+
+        </div>
+      </div>
 
     </div>
   </div>
@@ -21,7 +32,7 @@ export default {
     return {
         types: "bubbles",
       size:"30%",
-      show:"1"
+      show:""
     }
   }
 }
